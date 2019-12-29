5251694d159425d4c3b42f1c9e5ec0f94781b228
caldwell619 <caldwell8814@gmail.com>
2019-12-29 10:25:59 -0600

    creating font mapping

---
+++ web/src/data/config.js
@@ -0,0 +1,38 @@
+export const fontMapping = {
+  // def tokens
+  'scatter': 'A',
+  'redirect': 'B',
+  'brace': 'C',
+  'evade': 'D',
+  'contain': 'd',
+  // dice damages
+  'crit': 'E',
+  'damage': 'F',
+  'accuracy': 'G',
+  // command tokens
+  'navigation': 'M',
+  'squadrons': 'O',
+  'concentrateFire': 'P',
+  'repair': 'Q',
+  // upgrade icons
+  'ordnance': 'S',
+  'commander': 'T',
+  'officer': 'U',
+  'offensive-retrofit': 'V',
+  'title': 'W',
+  'weapons-team': 'X',
+  'weapons-team-2': 'X',
+  'ion-cannons': 'Y',
+  'support-team': 'Z',
+  'turbo-lasers': '[',
+  'defensive-retrofit': ']',
+  'fleet-support': 'f',
+  'fleet-command': 'f', // needs icon
+  // factions
+  'empire-alt': 'u',
+  'empire': 's',
+  'boba-fett': 't',
+  'rebel': 'r',
+  'republic': 'v',
+  'cis': 'w',
+}
