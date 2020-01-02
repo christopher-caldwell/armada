88ce8ca4b5f23bc7beadafe0cde2578d4b40c35e
Christopher Caldwell <christophercaldwell@swbc.com>
2020-01-01 23:28:04 -0600

    adding kebab formatter

--- web/src/views/user/ships/Upgrades.vue
+++ web/src/views/user/ships/Upgrades.vue
@@ -8,12 +8,9 @@
     v-row
       v-col.status-text
         h2 {{ statusText }}
-    v-row(v-if="showUpgrades")
+    v-row
       v-col(cols='6' md='3' v-for="(upgrade, index) in availableUpgrades" v-if="determineIsValidUpgrade(upgrade)")
         UpgradeCard(:upgrade="upgrade")
-    v-row(v-else)
-      v-col(align='center')
-        h2 Commander has already been chosen 
 </template>
 
 <script>
@@ -21,7 +18,7 @@ import FilterCards from '@/components/ships/FilterCards.vue'
 import SearchCards from '@/components/ships/SearchCards.vue'
 import UpgradeCard from '@/components/upgrades/UpgradeCard.vue'
 import { filterShips } from '@/utilities/filtering'
-import { capitalizeWord } from '@/utilities/formatting'
+import { convertKebabToUpper } from '@/utilities/formatting'
 import { determineCardsToBeShown } from '@/utilities/coreLogic'
 import { upgradeCards } from '@/data/cards'
 import { mapGetters } from 'vuex'
@@ -62,26 +59,26 @@ import { mapGetters } from 'vuex'
     },
     computed: {
       ...mapGetters('fleet', ['totalPoints', 'hasCommanderBeenChosen', 'targetShip']),
-      showUpgrades(){
-        return true  
-      },
       statusText(){
         if(this.targetShip){
-          const type = capitalizeWord(this.$route.query.type)
+          const type = convertKebabToUpper(this.$route.query.type)
           const shipTitle = this.targetShip.title
           return `Adding ${type} to ${shipTitle}`
         } else {
           return ''
         }
       },
-
     },
     methods: {
       updateSearchTerm(newTerm){
         this.availableUpgrades = filterShips(this.baseUpgradeSet, newTerm)
       },
       determineIsValidUpgrade(upgrade){
-        return this.totalPoints + upgrade.points < 400
+        const chosenFaction = this.$store.state.fleet.chosenFaction
+        const isValidPoints = this.totalPoints + upgrade.points < 400
+        // no faction, or matches the correct one
+        const isValidFaction = !upgrade.faction || upgrade.faction === chosenFaction
+        return isValidPoints && isValidFaction
       },
       updateCards(){
         const upgradeType = this.$route.query.type
